6c7a21c61e9ff34476a1a508e4d117bd83641b74
lutfiarifs <lutfiarifsyaputra@gmail.com>
2026-04-10 22:42:44 +0700

    Fix POM structure, paths, and update login data

--- cypress/e2e/tugas17-pom/login-pom.cy.js
+++ cypress/e2e/tugas17-pom/login-pom.cy.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import LoginPage from '../support/pages/LoginPage';
+import LoginPage from '../../support/pages/LoginPage';
 
 describe('OrangeHRM Login Feature with POM', () => {
   
@@ -9,76 +8,44 @@ describe('OrangeHRM Login Feature with POM', () => {
     cy.fixture('loginData').as('data');
   });
 
-  it('TC1 - Login dengan Username & Password valid', function() {
+  // Test Case 1 - Tes Login Username dan Password valid
+  it('Login dengan Username & Password valid', function() {
     LoginPage.login(this.data.validUser.username, this.data.validUser.password);
     cy.url().should('include', '/dashboard');
   });
 
-  it('TC2 - Login dengan Username salah', function() {
-    LoginPage.login(this.data.invalidUser.username, this.data.validUser.password);
+  // Test Case 2 - Tes Login dengan Ssername "test"
+  it('Login dengan Username "test', function() {
+    
+    // Mengisi kolom Username dengan "test" yang berarti username salah dari properti invalidUsername
+    LoginPage.login(this.data.invalidUsername.username, this.data.invalidUsername.password);
     LoginPage.errorMessage().should('have.text', 'Invalid credentials');
   });
 
-  it('TC3 - Login dengan Password salah', function() {
-    LoginPage.login(this.data.validUser.username, this.data.invalidUser.password);
+  // Test Case 3 - Tes Login dengan Password "admin321"
+  it('Login dengan Password "admin321"', function() {
+
+    // Mengisi kolom Password dengan "admin321" yang berarti username salah dari properti invalidPassword
+    LoginPage.login(this.data.invalidPassword.username, this.data.invalidPassword.password);
     LoginPage.errorMessage().should('have.text', 'Invalid credentials');
   });
 
-  it('TC4 - Login tanpa mengisi Username dan Password', function() {
-    LoginPage.loginButton().click();
-    LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
-  });
-
-  it('TC5 - Login dengan Username kosong', function() {
+  // Test Case 4 - Tes Login tanpa mengisi Username 
+  it('Login dengan Username kosong', function() {
     LoginPage.login(null, this.data.validUser.password);
     LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
   });
 
-  it('TC6 - Login dengan Password kosong', function() {
+  // Test Case 5 - Tes Login tanpa mengisi Password 
+  it('Login dengan Password kosong', function() {
     LoginPage.login(this.data.validUser.username, null);
     LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
   });
 
-=======
-import LoginPage from '../support/pages/LoginPage';
-
-describe('OrangeHRM Login Feature with POM', () => {
-  
-  beforeEach(() => {
-    LoginPage.visit();
-    // Mengambil data dari fixtures
-    cy.fixture('loginData').as('data');
-  });
-
-  it('TC1 - Login dengan Username & Password valid', function() {
-    LoginPage.login(this.data.validUser.username, this.data.validUser.password);
-    cy.url().should('include', '/dashboard');
-  });
-
-  it('TC2 - Login dengan Username salah', function() {
-    LoginPage.login(this.data.invalidUser.username, this.data.validUser.password);
-    LoginPage.errorMessage().should('have.text', 'Invalid credentials');
-  });
-
-  it('TC3 - Login dengan Password salah', function() {
-    LoginPage.login(this.data.validUser.username, this.data.invalidUser.password);
-    LoginPage.errorMessage().should('have.text', 'Invalid credentials');
-  });
-
-  it('TC4 - Login tanpa mengisi Username dan Password', function() {
+  // Test Case 6 - Tes Login tanpa mengisi Username dan Password
+  it('Login tanpa mengisi Username dan Password', function() {
     LoginPage.loginButton().click();
     LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
   });
 
-  it('TC5 - Login dengan Username kosong', function() {
-    LoginPage.login(null, this.data.validUser.password);
-    LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
-  });
-
-  it('TC6 - Login dengan Password kosong', function() {
-    LoginPage.login(this.data.validUser.username, null);
-    LoginPage.requiredMessage().should('be.visible').and('contain', 'Required');
-  });
-
->>>>>>> 80bd8f9f486386ae37b077042dc506c28f90a284
 });
--- cypress/support/pages/LoginPage.js
+++ cypress/support/pages/LoginPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class LoginPage {
   // Locators
   inputUsername() { return cy.get('input[name="username"]'); }
@@ -19,26 +18,4 @@ class LoginPage {
   }
 }
 
-=======
-class LoginPage {
-  // Locators
-  inputUsername() { return cy.get('input[name="username"]'); }
-  inputPassword() { return cy.get('input[name="password"]'); }
-  loginButton() { return cy.get('button[type="submit"]'); }
-  errorMessage() { return cy.get('.oxd-alert-content-text'); }
-  requiredMessage() { return cy.get('.oxd-input-group__message'); }
-
-  // Actions
-  visit() {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
-  }
-
-  login(username, password) {
-    if (username) this.inputUsername().type(username);
-    if (password) this.inputPassword().type(password);
-    this.loginButton().click();
-  }
-}
-
->>>>>>> 80bd8f9f486386ae37b077042dc506c28f90a284
 export default new LoginPage();
