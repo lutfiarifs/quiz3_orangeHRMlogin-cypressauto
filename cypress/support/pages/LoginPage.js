80bd8f9f486386ae37b077042dc506c28f90a284
lutfiarifs <lutfiarifsyaputra@gmail.com>
2026-04-10 21:35:15 +0700

    Add files via upload

---
+++ cypress/support/pages/LoginPage.js
@@ -0,0 +1,21 @@
+class LoginPage {
+  // Locators
+  inputUsername() { return cy.get('input[name="username"]'); }
+  inputPassword() { return cy.get('input[name="password"]'); }
+  loginButton() { return cy.get('button[type="submit"]'); }
+  errorMessage() { return cy.get('.oxd-alert-content-text'); }
+  requiredMessage() { return cy.get('.oxd-input-group__message'); }
+
+  // Actions
+  visit() {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  }
+
+  login(username, password) {
+    if (username) this.inputUsername().type(username);
+    if (password) this.inputPassword().type(password);
+    this.loginButton().click();
+  }
+}
+
+export default new LoginPage();
